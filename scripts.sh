98a92156311341b096f491e3dbce5f56e09746e3
sangbin <sbpark0611@kaist.ac.kr>
2023-08-07 13:03:29 +0900

    1

---
+++ scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH -J normal_short
+#SBATCH -N 1
+#SBATCH --mem=0
+#SBATCH -n 1
+#SBATCH -c 4
+#sbatch --gres=gpu:1
+#SBATCH -p jepyc
+#SBATCH -o %x.out
+#SBATCH -e %x.err
+#SBATCH -D /proj/internal_group/dscig/kdkyum/workdir/pydreamer-test
+
+__conda_setup="$('/opt/olaf/anaconda3/2020.11/GNU/4.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$__conda_setup"
+unset __conda_setup
+conda activate pydreamer
+export WANDB_MODE=offline
+export PYTHONPATH='.'
+
+python launch.py
