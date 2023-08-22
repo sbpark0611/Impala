946ad0397a16ecf36e78b1d55a2b3f0b53210868
sangbin <sbpark0611@kaist.ac.kr>
2023-08-22 23:32:55 +0900

    1

--- launch.py
+++ launch.py
@@ -6,7 +6,6 @@ import numpy as np
 import ray
 from ray import air, tune
 from ray.rllib.algorithms import impala
-from ray.rllib.utils.test_utils import check_learning_achieved
 from ray.tune import registry
 
 if __name__ == "__main__":
