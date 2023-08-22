931af9cc1d6e2dee5796c17cf487fec3d38465d8
sangbin <sbpark0611@kaist.ac.kr>
2023-08-22 23:29:00 +0900

    1

--- launch.py
+++ launch.py
@@ -1,34 +1,3 @@
-import argparse
-import os
-
-import numpy as np
-
-import ray
-from ray import air, tune
-from ray.rllib.algorithms import impala
-from ray.rllib.examples.env.look_and_push import LookAndPush, OneHot
-from ray.rllib.examples.env.repeat_after_me_env import RepeatAfterMeEnv
-from ray.rllib.examples.env.repeat_initial_obs_env import RepeatInitialObsEnv
-from ray.rllib.examples.env.stateless_cartpole import StatelessCartPole
-from ray.rllib.utils.framework import try_import_tf
-from ray.rllib.utils.test_utils import check_learning_achieved
-from ray.tune import registry
-from ray.tune.logger import pretty_print
-
-from memory_planning_game import MemoryPlanningGame
-from ray.air.integrations.mlflow import MLflowLoggerCallback
-from ray.train.torch import TorchCheckpoint, TorchPredictor
-import time
-from ray.air.integrations.mlflow import setup_mlflow
-import mlflow
-from memory_maze import tasks
-import wandb
-import flatdict
-
-from collections.abc import MutableMapping
-
-from ray.air.integrations.wandb import WandbLoggerCallback
-import shutil
 
 
 if __name__ == "__main__":
