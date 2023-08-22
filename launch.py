243e3da2740bac8462e026de03a70aefdd8aec80
sangbin <sbpark0611@kaist.ac.kr>
2023-08-22 23:33:24 +0900

    1

--- launch.py
+++ launch.py
@@ -6,7 +6,6 @@ import numpy as np
 import ray
 from ray import air, tune
 from ray.rllib.algorithms import impala
-from ray.tune import registry
 
 if __name__ == "__main__":
     print("hello world")
